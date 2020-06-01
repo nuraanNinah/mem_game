1df9980c8219407ebb3edd16c892c30e3618c3e3
nuraanNinah <60215982+nuraanNinah@users.noreply.github.com>
2020-06-01 12:30:13 +0200

    second commit

---
+++ mem_game/memory/spec/game-test.spec.js
@@ -0,0 +1,25 @@
+let card = require("../src/main")
+beforeEach,
+function () {
+
+	const dom = new jsDOM
+	describe('memory game tests', function () {
+
+		it('must have 12 cards', function () {
+			expect(game.card.length).toEqual(12)
+		});
+
+	})
+	it("should be able to add addEventListener to all html elements", function () {
+		let shuffle = require("../src/main")
+
+		expect(shuffle).toBeDefined();
+
+	})
+
+	it("should be able to add addEventListener to all html elements", function () {
+		let resetBoard = require("../src/main")
+
+		expect(resetBoard).toBeDefined();
+	});
+};
